2e93d2ece385f35c177da90ab47eb51542b5eb37
Beadko <beadko@gmail.com>
2025-02-18 17:02:28 +0700

    Initial setup

---
+++ exchanges/bitkub/bitkub.go
@@ -0,0 +1,21 @@
+package bitkub
+
+import (
+	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
+)
+
+// Bitkub is the overarching type across this package
+type Bitkub struct {
+	exchange.Base
+}
+
+const (
+	bitkubAPIURL     = ""
+	bitkubAPIVersion = ""
+
+	// Public endpoints
+
+	// Authenticated endpoints
+)
+
+// Start implementing public and private exchange API funcs below
---
+++ exchanges/bitkub/bitkub_test.go
@@ -0,0 +1,55 @@
+package bitkub
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/thrasher-corp/gocryptotrader/config"
+	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
+)
+
+// Please supply your own keys here to do authenticated endpoint testing
+const (
+	apiKey                  = ""
+	apiSecret               = ""
+	canManipulateRealOrders = false
+)
+
+var bi = &Bitkub{}
+
+func TestMain(m *testing.M) {
+	bi.SetDefaults()
+	cfg := config.GetConfig()
+	err := cfg.LoadConfig("../../testdata/configtest.json", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exchCfg, err := cfg.GetExchangeConfig("Bitkub")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exchCfg.API.AuthenticatedSupport = true
+	exchCfg.API.AuthenticatedWebsocketSupport = true
+	exchCfg.API.Credentials.Key = apiKey
+	exchCfg.API.Credentials.Secret = apiSecret
+
+	err = bi.Setup(exchCfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+// Ensures that this exchange package is compatible with IBotExchange
+func TestInterface(t *testing.T) {
+	var e exchange.IBotExchange
+	if e = new(Bitkub); e == nil {
+		t.Fatal("unable to allocate exchange")
+	}
+}
+
+// Implement tests for API endpoints below
