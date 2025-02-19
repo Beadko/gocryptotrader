220a31ed5857d1a96cb7a6bc21be3903f04659b2
Beadko <beadko@gmail.com>
2025-02-19 10:51:23 +0700

    Add REST API endpoints

--- exchanges/bitkub/bitkub.go
+++ exchanges/bitkub/bitkub.go
@@ -10,12 +10,46 @@ type Bitkub struct {
 }
 
 const (
-	bitkubAPIURL     = ""
-	bitkubAPIVersion = ""
+	bitkubAPIURL     = "https://api.bitkub.com/api"
+	bitkubAPIVersion = "3"
 
 	// Public endpoints
+	bitkubTicker     = "market/ticker"
+	bitkubBids       = "market/bids"
+	bitkubAsks       = "market/asks"
+	bitkubDepth      = "market/depth"
+	bitkubTrades     = "market/trades"
+	bitkubServerTime = "market/servertime"
 
 	// Authenticated endpoints
+
+	// User endpoints
+	bitkubTradingCredits = "user/trading-credits" // #nosec G101
+	bitkubLimits         = "user/limits"
+
+	// Market endpoints
+	bitkubBalances         = "market/balances"
+	bitkubPlaceBid         = "market/place-bid"
+	bitkubPlaceAsk         = "market/place-ask"
+	bitkubPCancelOrder     = "market/cancel-order"
+	bitkubWsToken          = "market/wstoken"
+	bitkubMyOpenOrders     = "market/my-open-orders"
+	bitkubWsMyOrderHistory = "market/my-order-history"
+	bitkubOrderInfo        = "market/order-info"
+
+	// Crypto endpoints
+	bitkubInternalWithdraw      = "crypto/internal-withdraw"
+	bitkubAddresses             = "crypto/addresses"
+	bitkubCryptoWithdraw        = "crypto/withdraw"
+	bitkubCryptoDepositHistory  = "crypto/deposit-history"
+	bitkubCryptoWithdrawHistory = "crypto/withdraw-history"
+	bitkubGenerateAddress       = "crypto/generate-address"
+
+	// Fiat endpoints
+	bitkubFiatAccounts        = "fiat/accounts"
+	bitkubFiatWithdraw        = "fiat/withdraw"
+	bitkubFiatDepositHistory  = "fiat/deposit-history"
+	bitkubFiatWithdrawHistory = "fiat/withdraw-history"
 )
 
 // Start implementing public and private exchange API funcs below
